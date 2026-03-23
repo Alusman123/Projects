61a04ffcc83df9377017d6166c7d990f4e999c14
Alusman123 <alyas3933@gmail.com>
2026-03-23 17:35:51 +0800

    Fist Commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+   <div className="flex flex-row bg-amber-300">
+    <div className="px-1">
+      sup
+    </div>
+    <div>hello</div>
+   </div>
+  )
+}
+
+export default App
