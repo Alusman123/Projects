556fb96900c0ded637346d4c335df48f839da658
Alusman123 <alyas3933@gmail.com>
2026-04-22 21:42:53 +0800

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import './App.css'
 import { Mainpage } from './Screen/MainPage';
 import { Header } from './Screen/HeaderContent';
 import { Landing } from './Screen/LandingPage';
-import TopoBg from './assets/topgif.mp4';
 
 type Section = "about" | "projects" | "techstack" | "experience" | null;
 
@@ -14,8 +13,6 @@ function App() {
   return (
     <div>
       <div className="relative min-h-screen w-full flex items-center justify-center">
-        
-       
 
         <header className='bg-black absolute top-0 left-0 z-50 flex items-center justify-between border-b-[1.5px] border-white rounded-b-[40px] w-full h-[85px] px-8'>
           <Header
